fd23b9ec147da05236b8cfa52fab04360d1e1d4f
Medhavi Basera <1905781@kiit.ac.in>
2021-11-25 11:58:35 +0530

    Header added

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Headerimg from "../../assets/meals.jpeg";
+import HeaderCardButton from "./HeaderCartButton";
 import classes from "./Header.module.css";
 export default function Header() {
   return (
     <>
       <header className={classes.header}>
         <h1>Mealz</h1>
-        <button>Cart</button>
+        <HeaderCardButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={Headerimg} alt="table full of delicious food" />
